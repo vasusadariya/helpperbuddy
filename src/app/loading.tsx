cb006cb81840a035fca8b31c153a75f4433e8b93
sadariya vasu <sadariyavasu@Vasus-Macbook-AIr.local>
2025-02-16 16:15:13 +0530

    solved Rive errors

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,15 @@
-'use client'
-import RiveWrapper from "@/components/ui/RiveWrapper";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const RiveWrapper = dynamic(() => import("@/components/ui/RiveWrapper"), {
+  ssr: false, // Ensure it's only loaded on the client-side
+});
 
 export default function App() {
-
   return (
     <div className="App">
-        <RiveWrapper />
+      <RiveWrapper />
     </div>
   );
 }
--- src/components/ui/RiveWrapper.tsx
+++ src/components/ui/RiveWrapper.tsx
@@ -1,17 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
 
-// Wrapper component to isolate useRive logic that
-// renders the <RiveComponent />
 const RiveWrapper = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
   const { RiveComponent } = useRive({
-    src: "loader_icon.riv",
+    src: "/loader_icon.riv", 
     stateMachines: "State Machine 1",
     autoplay: true,
     layout: new Layout({
       fit: Fit.Cover,
-      alignment: Alignment.Center
-    })
+      alignment: Alignment.Center,
+    }),
   });
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false); // Cleanup on unmount
+  }, []);
+
+  if (!isMounted || !RiveComponent) return null; // Prevent abort error
 
   return <RiveComponent />;
 };
