91ddfa47d469c436b362d3f626101caff2896d69
aryanjsawant <aryan.j.sawant@gmail.com>
2025-02-19 20:18:58 +0530

    blog

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,28 +1,47 @@
-import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
 const PAGE_SIZE = 9;
 
+export async function POST(req: Request) {
+  try {
+    const { id } = await req.json();
+
+    if (id) {
+      const blog = await prisma.blog.findUnique({
+        where: { id, isActive: true },
+      });
+
+      if (!blog) return NextResponse.json({ error: "Blog not found" }, { status: 404 });
+      return NextResponse.json(blog);
+    }
+
+    return NextResponse.json({ error: "Missing blog ID" }, { status: 400 });
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid request: " +error }, { status: 400 });
+  }
+}
+
 export async function GET(req: Request) {
-	const { searchParams } = new URL(req.url);
-	const page = Number(searchParams.get('page')) || 1;
-	const take = PAGE_SIZE;
-	const skip = (page - 1) * take;
+  const { searchParams } = new URL(req.url);
+  const page = Number(searchParams.get("page")) || 1;
+  const take = PAGE_SIZE;
+  const skip = (page - 1) * take;
 
-	const results = await prisma.blog.findMany({
-		take,
-		skip,
-		where: { isActive: true },
-		orderBy: { createdAt: 'desc' },
-	});
+  const results = await prisma.blog.findMany({
+    take,
+    skip,
+    where: { isActive: true },
+    orderBy: { createdAt: "desc" },
+  });
 
-	const total = await prisma.blog.count({ where: { isActive: true } });
+  const total = await prisma.blog.count({ where: { isActive: true } });
 
-	return NextResponse.json({
-		data: results,
-		metadata: {
-			hasNextPage: skip + take < total,
-			totalPages: Math.ceil(total / take),
-		},
-	});
+  return NextResponse.json({
+    data: results,
+    metadata: {
+      hasNextPage: skip + take < total,
+      totalPages: Math.ceil(total / take),
+    },
+  });
 }
